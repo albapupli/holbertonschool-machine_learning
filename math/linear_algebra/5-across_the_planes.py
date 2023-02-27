d6f0955fe526853df9adca2409523202212d7506
albapupli <apupli19@epoka.edu.al>
2023-02-27 06:43:11 -0800

    ex 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,7 +4,9 @@
 
 def add_matrices2D(mat1, mat2):
     """add matrices"""
+    row = len(mat1)
+    col = len(mat1[0])
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    sum = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1))] for i in range(len(mat1[0]))]
+    sum = [[mat1[i][j] + mat2[i][j] for j in range(row)] for i in range(col)]
     return sum
