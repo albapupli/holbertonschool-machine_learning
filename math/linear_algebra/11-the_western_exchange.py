535a6d605f0e077fd02c85cff53e39140b32f7ec
albapupli <apupli19@epoka.edu.al>
2023-02-27 16:18:22 -0800

    ex 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function to transpose a matrix"""
+import numpy as np
+def np_transpose(matrix):
+    """transpose of matrix"""
+    result = np.transpose(matrix)
+    return result
