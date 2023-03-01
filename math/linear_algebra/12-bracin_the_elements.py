8097b224b000d06bf07e32cafa592af374dcef49
albapupli <apupli19@epoka.edu.al>
2023-03-01 15:50:15 -0800

    ex 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,7 +2,7 @@
 """find element wise of a matrix"""
 
 
-import numpy as np
+
 def np_elementwise(mat1, mat2):
     """add subtr mul div matrices"""
-    return np.add(mat1, mat2), np.subtract(mat1, mat2), np.multiply(mat1, mat2), np.divide(mat1, mat2)
+    return (mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2)
