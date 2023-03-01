a3adabbaaed9d423894e544883c5017c9837427f
albapupli <apupli19@epoka.edu.al>
2023-03-01 15:41:39 -0800

    ex 8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -6,7 +6,7 @@ def mat_mul(mat1, mat2):
     """multiply 2 matrices together"""
     if len(mat1[0]) != len(mat2):
         return None
-    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
