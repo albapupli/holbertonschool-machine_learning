eddbf6dccfc4bc0f18d05214d99dfb0b049bbd2e
albapupli <apupli19@epoka.edu.al>
2023-03-01 15:51:51 -0800

    ex 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -5,4 +5,5 @@
 import numpy as np
 def np_cat(mat1, mat2, axis=0):
     """concat two matrices"""
-    return np.concatenate([mat1, mat2], axis)
+    return np.concatenate((mat1, mat2), axis=axis)
+
