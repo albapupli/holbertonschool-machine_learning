0ee6bbec3074dcc83728d188b8b18a38110a6ca1
albapupli <apupli19@epoka.edu.al>
2023-03-01 15:30:00 -0800

    ex 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,4 +5,14 @@
 import numpy as np
 def cat_matrices2D(mat1, mat2, axis=0):
     """concat matrices"""
-    return np.concatenate((mat1,mat2),axis)
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        result = mat1 + mat2
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        result = [mat1[i] + mat2[i] for i in range(len(mat1))]
+    else:
+        return None
+    return result
