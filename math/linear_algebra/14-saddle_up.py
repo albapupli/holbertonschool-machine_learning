2515546ba78082d960e29cb928f9f0095e2e4465
albapupli <apupli19@epoka.edu.al>
2023-03-01 15:57:58 -0800

    ex 14

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """function concat two matrices"""
+
+
 import numpy as np
+
+
 def np_cat(mat1, mat2, axis=0):
     """concat two matrices"""
-    return np.concatenate([mat1, mat2], axis)
+    return (np.matmul(mat1, mat2)) # mat1.matmul(mat2)
