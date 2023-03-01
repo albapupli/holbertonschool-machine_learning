459fd54c97b4b28dd82477eca1c9aa5adf028e75
albapupli <apupli19@epoka.edu.al>
2023-03-01 15:46:57 -0800

    ex 10

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """function to find shape of matrix"""
+
+
 import numpy as np
 def np_shape(matrix):
     """find the shapw"""
-    return np.shape(matrix)
+    return matrix.shape
