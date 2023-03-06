6fe3398f7498772a82460c61a4b0317b3ab16b3e
albapupli <apupli19@epoka.edu.al>
2023-03-06 08:37:19 -0800

    ex 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -3,11 +3,19 @@
 
 
 def poly_integral(poly, C=0):
-    """calculate integral of poly"""
-    if(poly == 0):
-        return 0
-    integral = []
-    integral.append(0)
-    for i in range(0, len(poly)):
-        integral.append(poly[i] / (i+1))
-    return integral
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    elif type(C) is int:
+        if poly == [0]:
+            return C
+        exponent = 0
+        integral = poly.copy()
+        for i in range(len(integral)):
+            if type(integral[i]) is int or type(integral[i]) is float:
+                exponent +=1
+                number = integral[i] / exponent
+                integral[i] = int(number) if number % 1 == 0 else number
+            else:
+                return None
+        integral.insert(0, C)
+        return integral
