101efd742db596ebb721721eaf90b069f6f3b133
albapupli <apupli19@epoka.edu.al>
2023-03-06 11:54:46 -0800

    ex 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,7 +4,11 @@
 
 def poly_derivative(poly):
     """calculate the der"""
+    if type(poly) is not list or poly == []:
+        return None
+    elif len(poly) < 2:
+        return [0]
     der = []
     for i in range(1, len(poly)):
-        der.append(poly[i] * i)
+          der.append(poly[i] * i)
     return der
