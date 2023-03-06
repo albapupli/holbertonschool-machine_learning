ef1964c5524d45951e49b485457c8feebbc4be0e
albapupli <apupli19@epoka.edu.al>
2023-03-06 06:59:32 -0800

    ex 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """sum of n consecutive nr"""
-    if(type(n) != int || n < 1):
+    if type(n) != int or n < 1:
         return None
     return (int)(n*(n+1)*(2*n+1)/6)
