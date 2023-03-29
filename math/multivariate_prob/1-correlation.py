1716557267a7f070aa82724b32547f949194bbc1
albapupli <apupli19@epoka.edu.al>
2023-03-29 13:43:23 -0700

    ex1

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -7,7 +7,7 @@ def correlation(C):
     Calculates a correlation matrix
     """
     if type(C) is not np.ndarray:
-        TypeError("C must be a numpy.ndarray")
+        raise TypeError("C must be a numpy.ndarray")
     if (len(C.shape) != 2) or (C.shape[0] != C.shape[1]):
         raise ValueError("C must be a 2D square matrix")
     D = np.sqrt(np.diag(C))
