b70560eca5591481460c34460ed5722966a050db
albapupli <apupli19@epoka.edu.al>
2023-03-29 13:22:57 -0700

    ex0

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -8,10 +8,10 @@ import numpy as np
 def mean_cov(X):
     """check here all the edge cases as denotes in the requirements"""
 
-    if X is not np.ndarray or len(x.shape[0]) != 2:
+    if not isinstance(X, np.ndarray) or len(x.shape[0]) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-
-    if len(X.shape) < 2:
+    n, d = X.shape
+    if n < 2:
         raise ValueError("X must contain multiple data points")
 
     mean = np.mean(X, axis=0)
