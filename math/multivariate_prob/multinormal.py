bc439faebf9b68807399e14911844f6bd5d98a70
albapupli <apupli19@epoka.edu.al>
2023-03-29 15:55:30 -0700

    ex2

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -11,10 +11,10 @@ class MultiNormal:
         """
         the construction method
         """
-        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+        if type(data) is not np.ndarray or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
         n, d = data.shape
-        if n < 2 or n = 1:
+        if n < 2:
             raise ValueError("data must contain multiple data points")
         self.mean, self.cov = mean_cov(data.T)
         self.mean = self.mean.reshape(-1, 1)
