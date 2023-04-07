54a9292261603c748a5d5e3f1c28a6f0c72cd9c0
albapupli <apupli19@epoka.edu.al>
2023-04-07 15:55:16 -0700

    ex1

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """a class representing a neuron based on previous task"""
 
+
 import numpy as np
+
 
 class Neuron:
     """
@@ -13,20 +15,19 @@ class Neuron:
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        
+            raise ValueError("nx must be a positive integer")        
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
         @property
         def W(self):
-            return (self.__W)
+            return self.__W
 
         @property
         def b(self):
-            return (self.__b)
+            return self.__b
 
         @property
         def A(self):
-            return (self.__A)
+            return self.__A
