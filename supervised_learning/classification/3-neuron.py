9bfd0af66105891b251dd2d5409b8aea8fc1c68c
albapupli <apupli19@epoka.edu.al>
2023-04-07 17:30:06 -0700

    ex4

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -20,33 +20,32 @@ class Neuron:
         self.__b = 0
         self.__A = 0
 
-        @property
-        def W(self):
-            return (self.__W)
+    @property
+    def W(self):
+        return (self.__W)
 
-        @property
-        def b(self):
-            return (self.__b)
+    @property
+    def b(self):
+        return (self.__b)
 
-        @property
-        def A(self):
-            return (self.__A)
+    @property
+    def A(self):
+        return (self.__A)
 
-        def forward_prop(self, X):
-            """
-            calculates the forward propagation of the neuron
-            """
-            z = np.matmul(self.W, X) + self.b
-            self.__A = 1 / (1 + (np.exp(-z)))
-            return (self.A)
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neuron
+        """
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return (self.A)
 
-        def cost(self, Y, A):
-            """
-            calculates the cost of the model using
-            logistic regression function:
-            """
-            m = Y.shape[1]
-            m_loss = np.sum((Y * np.log(A)) +
-                            ((1 - Y) * np.log(1.0000001 - A)))
-            cost = (1 / m) * (-(m_loss))
-            return (cost)
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using
+        logistic regression function:
+        """
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
