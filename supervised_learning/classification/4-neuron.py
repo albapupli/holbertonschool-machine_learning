b747b46341576d610182458a8f7a745b8741e10b
albapupli <apupli19@epoka.edu.al>
2023-04-07 17:27:44 -0700

    ex4

--- supervised_learning/classification/4-neuron.py
+++ supervised_learning/classification/4-neuron.py
@@ -18,45 +18,43 @@ class Neuron:
         self.__b = 0
         self.__A = 0
 
-        @property
-        def W(self):
-            return (self.__W)
+    @property
+    def W(self):
+        return (self.__W)
 
-        @property
-        def b(self):
-            return (self.__b)
+    @property
+    def b(self):
+        return (self.__b)
 
-        @property
-        def A(self):
-            return (self.__A)
+    @property
+    def A(self):
+        return (self.__A)
 
-        def forward_prop(self, X):
-            """
-            calculates the forward propagation of the neuron
-            """
-            z = np.matmul(self.W, X) + self.b
-            self.__A = 1 / (1 + (np.exp(-z)))
-            return (self.A)
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neuron
+        """
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return (self.A)
 
-        def cost(self, Y, A):
-            """
-            calculates the cost of the model using
-            logistic regression function:
-            loss = -((Y * log(A)) + ((1 - Y) * log(1 - A)))
-            logistic regression cost function:
-            cost = (1 / m) * sum of loss function for all m example
-            """
-            m = Y.shape[1]
-            m_loss = np.sum((Y * np.log(A)) +
-                            ((1 - Y) * np.log(1.0000001 - A)))
-            cost = (1 / m) * (-(m_loss))
-            return (cost)
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using logistic regression function:
+        loss = -((Y * log(A)) + ((1 - Y) * log(1 - A)))
+        logistic regression cost function:
+        cost = (1 / m) * sum of loss function for all m example
+        """
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
 
-        def evaluate(self, X, Y):
-            """
-            evaluates the neuron's predictions
-            """
-            A = self.forward_prop(X)
-            cost = self.cost(Y, A)
-            prediction = np.where(A >= 0.5, 1, 0)
-            return (prediction, cost)
+    def evaluate(self, X, Y):
+        """
+        evaluates the neuron's predictions
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return (prediction, cost)
