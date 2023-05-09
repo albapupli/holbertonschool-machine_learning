cdbaf15c2926341a4acb8d1e454c24375e783a74
albapupli <apupli19@epoka.edu.al>
2023-05-09 21:57:26 +0200

    tensorflow ex

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -65,3 +65,4 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
         print("\tValidation Cost: {}".format(loss_valid))
         print("\tValidation Accuracy: {}".format(accuracy_valid))
         return saver.save(sess, save_path)
+    
