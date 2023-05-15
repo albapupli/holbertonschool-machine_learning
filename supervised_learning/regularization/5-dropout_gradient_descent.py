c422a114c49017abc0e86af281226f53cc1d68e4
albapupli <apupli19@epoka.edu.al>
2023-05-15 23:34:35 +0200

    ex5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-this function updates the weights of a neural network with Dropout regularization using gradient descent
+this function updates the weights
+of a neural network with Dropout regularization using gradient descent
 """
 
 
@@ -13,7 +14,7 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
     m = Y.shape[1]
     back = {}
-    
+
     for index in range(L, 0, -1):
         A = cache["A{}".format(index - 1)]
         if index == L:
@@ -25,12 +26,12 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
             # retreive previous dz and current activation
             dz_prev = back["dz{}".format(index + 1)]
             A_current = cache["A{}".format(index)]
-            
+
             # calculate current dz
             back["dz{}".format(index)] = (
                 np.matmul(W_prev.transpose(), dz_prev) *
                 (A_current * (1 - A_current)))
-            
+
             # multiply by dropout mask and divide by keep probability
             dz = back["dz{}".format(index)]
             dz *= cache["D{}".format(index)]
@@ -39,12 +40,12 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         # calculate weights and bias
         dW = (1 / m) * (np.matmul(dz, A.transpose()))
         db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        
+
         # store for next interation
         W_prev = weights["W{}".format(index)]
-        
+    
         # modify weights and bias
         weights["W{}".format(index)] = (
-        weights["W{}".format(index)] - (alpha * dW))
+            weights["W{}".format(index)] - (alpha * dW))
         weights["b{}".format(index)] = (
-        weights["b{}".format(index)] - (alpha * db))
+            weights["b{}".format(index)] - (alpha * db))
