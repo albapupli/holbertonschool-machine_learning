444a37b0e383b2f76e5cb6b5a75cfb3702382058
albapupli <apupli19@epoka.edu.al>
2023-05-15 23:07:47 +0200

    ex7

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -3,6 +3,7 @@
 this function determines if you should stop gradient descent early
 """
 
+
 def early_stopping(cost, opt_cost, threshold, patience, count):
     """
     check if the epoke time is here
@@ -11,8 +12,9 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
         count = 0
     else:
         count += 1
-    
+
     if count != patience:
         return False, count
     else:
         return True, count
+    
