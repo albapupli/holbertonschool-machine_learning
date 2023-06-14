80606ef9afd1066511da121f6f66556ec0ce3498
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 18:51:22 +0200

    ex2

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -8,10 +8,12 @@ import numpy as np
 
 
 def shuffle_data(X, Y):
-    
+    """
+    this function shuffles the data identically
+    """    
     m = X.shape[0]
     permutation = np.random.permutation(m)
     X_shuffled = X[permutation]
     Y_shuffled = Y[permutation]
-    
+
     return X_shuffled, Y_shuffled
