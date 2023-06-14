b51298463241f9a173f2fbd5481c17b28b3aa1a0
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 19:19:45 +0200

    ex10

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -1,9 +1,20 @@
-import numpy as np
+#!/usr/bin/env python3
+"""
+this function creates a training operation for nn
+using Adam optimization alg
+"""
+
+
 import tensorflow as tf
+
+
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    training operation of nn using Adam opt
+    """
     optimizer = tf.keras.optimizers.Adam(
-        learning_rate=alpha, 
-        beta_1=beta1, 
-        beta_2=beta2, 
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
         epsilon=epsilon)
     return optimizer.minimize(loss)
