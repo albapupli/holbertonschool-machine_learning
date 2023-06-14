2aaf8cd41a8819b3bb3ce8fc92b41ef007fd11fd
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 19:00:49 +0200

    ex6

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,6 +1,18 @@
+#!/usr/bin/env python3
+"""
+this function creates the training operation for a
+neural network in tf using gradient descent with momentum
+optimization alg
+"""
+
+
 import tensorflow as tf
 
+
 def create_momentum_op(loss, alpha, beta1):
+    """
+    training operation of nn with tf
+    """
     optimizer = tf.train.MomentumOptimizer(alpha, momentum=beta1)
     train_op = optimizer.minimize(loss)
     return train_op
