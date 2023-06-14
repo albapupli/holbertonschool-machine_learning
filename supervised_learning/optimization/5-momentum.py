e12ded2ea42bb97b98a3e6f0bb8ac6f727760e5c
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 18:58:24 +0200

    ex5

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python3
+"""
+this function updates a var using gradient descent with
+momentum optimization alg
+"""
+
+
 def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    updating a var using gradient descent with momentum optimization
+    """
     dW_prev = (beta1 * v) + ((1 - beta1) * grad)
     var -= (alpha * dW_prev)
     return var, dW_prev
