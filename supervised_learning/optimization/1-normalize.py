4252e6963ccf31b60aa593063753067c807c8727
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 18:49:16 +0200

    ex1

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -3,9 +3,11 @@
 this function normalizes a matrix
 """
 
+
 def normalize(X, m, s):
-    
-    # Normalize X by subtracting the mean and dividing by the standard deviation
+    """
+    Normalize X by subtracting the mean and dividing by the standard deviation
+    """
     X_norm = (X - m) / s
-    
+
     return X_norm
