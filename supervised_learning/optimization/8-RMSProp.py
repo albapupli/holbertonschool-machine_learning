ce5e0e908335fe2efcd276c8d900552e40c6baa4
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 19:10:25 +0200

    ex8

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -8,7 +8,8 @@ import tensorflow as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
-   
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                          decay=beta2, epsilon=epsilon)
     train_op = optimizer.minimize(loss)
     return train_op
