beccb4e7c54ed131317ebcce346239b80f7f3da5
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 19:06:00 +0200

    ex8

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
+"""
+this function calculates the weighted moving avg of dataset
+"""
+
+
 import numpy as np
 import tensorflow as tf
+
+
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
    
     optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
