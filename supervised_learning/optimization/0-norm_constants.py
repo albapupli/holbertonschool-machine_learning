e70a362436dfc44bf2e610177fef32c2bae71721
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 18:46:50 +0200

    ex0

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def normalization_constants(X):
     """
-    Calculates the mean and standard deviation of each 
+    Calculates the mean and standard deviation of each
     feature in the input matrix X.
     """
     mean = np.mean(X, axis=0)
