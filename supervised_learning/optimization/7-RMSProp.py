238cef2c9391a46c1d5bad08437ca44173733845
AlbaPupli <apupli19@epoka.edu.al>
2023-06-14 19:02:49 +0200

    ex7

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -1,5 +1,13 @@
-def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+#!/usr/bin/env python3
+"""
+this function updates a var using RMSProp optimization alg
+"""
 
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    updating a var using PMSProp
+    """
     s_dW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
     var -= alpha * (grad / (epsilon + (s_dW ** (1 / 2))))
     return var, s_dW
