e1ad4af57cbeca10c591abd291cf51f1149731c8
AlbaPupli <apupli19@epoka.edu.al>
2023-06-19 16:18:56 +0200

    ex11

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+"""
+this function updates the learning rate using inverse time decay in numpy
+"""
+
+
+import tensorflow as tf
+
+
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    updating learning rate via reverse time decay
+    """
     updated_alpha = alpha / (1 + decay_rate * (global_step // decay_step))
     return updated_alpha
