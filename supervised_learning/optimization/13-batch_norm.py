3bb99b7170b2f519ac0d10192c3e9741a04a2f08
AlbaPupli <apupli19@epoka.edu.al>
2023-06-19 16:38:13 +0200

    ex13

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python3
+"""
+this function  normalizes an unactivated output
+of a neural network using batch normalization
+"""
+
+
 import numpy as np
+
+
 def batch_norm(Z, gamma, beta, epsilon):
+    """
+    normalizing unactive nn output via batch normalization
+    """
     m, n = Z.shape
     mu = np.mean(Z, axis=0)
     var = np.var(Z, axis=0)
     Z_norm = (Z - mu) / np.sqrt(var + epsilon)
     Z_norm_scaled  = gamma * Z_norm + beta
-    return Z_norm_scaled 
+    return Z_norm_scaled
