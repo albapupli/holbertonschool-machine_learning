7ca666d5e8d6e9244520b0c990ce7565747b55ef
AlbaPupli <apupli19@epoka.edu.al>
2023-06-19 17:39:54 +0200

    ex3

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -11,7 +11,8 @@ def specificity(confusion):
     """
     Calculate the true negative rate (specificity) for each class
     """
-    tn = np.sum(confusion) - np.sum(confusion, axis=0) - np.sum(confusion, axis=1) + np.diag(confusion)
+    tn = np.sum(confusion) - np.sum(confusion, axis=0) - np.sum(
+        confusion, axis=1) + np.diag(confusion)
     fp = np.sum(confusion, axis=0) - np.diag(confusion)
     tnr = tn / (tn + fp)
 
