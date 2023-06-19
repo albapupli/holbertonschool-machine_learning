95ce41d092d41d9c7a42e9c39bd8bfebfc037a33
AlbaPupli <apupli19@epoka.edu.al>
2023-06-19 16:24:07 +0200

    ex12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -1,5 +1,15 @@
-import numpy as np
+#!/usr/bin/env python3
+"""
+this function  creates a learning rate decay operation in tensorflow using inverse time decay
+"""
+
+
 import tensorflow as tf
+
+
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    creating learning rate decay via inverse time decay in tf
+    """
     alpha_new = tf.divide(alpha, tf.pow(1 + decay_rate * tf.floor_div(global_step, decay_step), 1))
     return alpha_new
