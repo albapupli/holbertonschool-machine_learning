8e5aa1d35db88ee74e05d76ec07fd77fd434b8a2
AlbaPupli <apupli19@epoka.edu.al>
2023-06-19 16:40:00 +0200

    ex13

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -16,5 +16,5 @@ def batch_norm(Z, gamma, beta, epsilon):
     mu = np.mean(Z, axis=0)
     var = np.var(Z, axis=0)
     Z_norm = (Z - mu) / np.sqrt(var + epsilon)
-    Z_norm_scaled  = gamma * Z_norm + beta
+    Z_norm_scaled = gamma * Z_norm + beta
     return Z_norm_scaled
