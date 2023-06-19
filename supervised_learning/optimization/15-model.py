59f508554fddf3e1b5cad0f645a3741b7c88bd9d
AlbaPupli <apupli19@epoka.edu.al>
2023-06-19 17:20:08 +0200

    ex15

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -11,8 +11,9 @@ import tensorflow as tf
 
 
 def model(Data_train, Data_valid, layers, activations,
-          alpha=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, 
-          decay_rate=1, batch_size=32, epochs=5, save_path='./model.ckpt'):
+          alpha=0.001, beta1=0.9, beta2=0.999,
+          epsilon=1e-8, decay_rate=1, batch_size=32,
+          epochs=5, save_path='./model.ckpt'):
     """
     building, training and saving nn model in tf via != optimizations
     """
@@ -28,8 +29,8 @@ def model(Data_train, Data_valid, layers, activations,
     for i, layer_size in enumerate(layers):
         activation = activations[i]
         if i == 0:
-            model.add(tf.keras.layers.Dense(layer_size, input_shape=input_shape,
-                                            activation=activation))
+            model.add(tf.keras.layers.Dense(layer_size,
+                                            input_shape=input_shape, activation=activation))
         else:
             model.add(tf.keras.layers.Dense(layer_size, activation=activation))
 
@@ -42,14 +43,15 @@ def model(Data_train, Data_valid, layers, activations,
 
     metric = tf.keras.metrics.SparseCategoricalAccuracy()
 
-    lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch: alpha /
-                                                        (1 + decay_rate * epoch))
+    lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch:
+                                                            alpha / (1 + decay_rate * epoch))
     model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=save_path,
                                                           save_best_only=True)
 
     model.compile(optimizer=optimizer, loss=loss_fn, metrics=[metric])
 
     history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
-                        validation_data=(X_valid, y_valid), callbacks=[lr_scheduler, model_checkpoint])
+                        validation_data=(X_valid, y_valid), callbacks=[
+                            lr_scheduler, model_checkpoint])
 
     return save_path
