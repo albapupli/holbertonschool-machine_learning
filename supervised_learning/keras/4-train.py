7785831e69ebddabc6af74b2a8fc6be45e34d1da
AlbaPupli <apupli19@epoka.edu.al>
2023-06-20 12:16:49 +0200

    ex4

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+this function trains a model using mini-batch gradient descent
+"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """
+    training using mini-batch gradient descent
+    """
+    history = network.fit(x=data, y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle)
+    return history
