e94e1eda900360806bdd1b6462cb98b8717b99ff
albapupli <57274211+albapupli@users.noreply.github.com>
2023-07-09 16:34:52 +0200

    ex0

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -8,7 +8,23 @@ from tensorflow.keras.models import load_model
 
 
 class Yolo:
-    
+    """Uses You only look once (YOLO)v3 to perform object detection
+    Args:
+        model_path: path to where a Darknet Keras model is stored
+        classes_path: path to where the list of class names used for the
+            Darknet model, listed in order of index, can be found
+        class_t: float representing the box score threshold for the
+            initial filtering step
+        nms_t: float representing the IOU threshold for non-max
+            suppression
+        anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2)
+            containing all of the anchor boxes:
+            outputs: is the number of outputs (predictions) made by the
+                Darknet model
+            anchor_boxes: is the number of anchor boxes used for each
+                prediction
+            2 => [anchor_box_width, anchor_box_height]
+    """
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = load_model(model_path)
         with open(classes_path) as f:
