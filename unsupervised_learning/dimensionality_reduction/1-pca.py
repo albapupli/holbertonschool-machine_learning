9401252cf690989cdb108bb15be549e962b703c0
albapupli <apupli19@epoka.edu.al>
2023-09-25 20:31:49 +0200

    ex1

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+a function  that performs PCA on a dataset
+"""
+
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Calculate the covariance matrix of X
+    """
+    cov_matrix = np.cov(X, rowvar=False)
+
+    # Calculate eigenvalues and eigenvectors
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+
+    # Sort eigenvalues and eigenvectors in descending order
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+
+    # Select the top ndim eigenvectors
+    top_eigenvectors = eigenvectors[:, :ndim]
+
+    # Compute the transformed data
+    T = np.dot(X, top_eigenvectors)
+
+    return T
