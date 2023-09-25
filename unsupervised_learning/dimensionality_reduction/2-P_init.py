55fefc7478481fd8ccef116edeef4ed7cba60fff
albapupli <apupli19@epoka.edu.al>
2023-09-25 20:39:20 +0200

    ex2

--- unsupervised_learning/dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-function that initializes all variables required to calculate the P affinities in t-SNE
+function that initializes all variables required to calculate
+the P affinities in t-SNE
 """
 
 
@@ -12,17 +13,17 @@ def P_init(X, perplexity):
     p affinities in t sne calculated
     """
     n, d = X.shape
-    
+
     # Calculate the squared pairwise distance matrix D
     D = np.zeros((n, n))
     for i in range(n):
         for j in range(n):
             if i != j:
                 D[i][j] = np.sum((X[i] - X[j])**2)
-    
+
     # Initialize P affinities matrix to zeros
     P = np.zeros((n, n))
-    
+
     # Initialize beta values to 1.0 for all data points
     betas = np.ones((n, 1))
 
