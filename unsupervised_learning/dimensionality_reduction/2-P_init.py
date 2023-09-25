576ecb783d02ccb89de1dda0a98f23664e08f39d
albapupli <apupli19@epoka.edu.al>
2023-09-25 20:37:32 +0200

    ex2

---
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+function that initializes all variables required to calculate the P affinities in t-SNE
+"""
+
+
+import numpy as np
+
+
+def P_init(X, perplexity):
+    """
+    p affinities in t sne calculated
+    """
+    n, d = X.shape
+    
+    # Calculate the squared pairwise distance matrix D
+    D = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                D[i][j] = np.sum((X[i] - X[j])**2)
+    
+    # Initialize P affinities matrix to zeros
+    P = np.zeros((n, n))
+    
+    # Initialize beta values to 1.0 for all data points
+    betas = np.ones((n, 1))
+
+    # Calculate the Shannon entropy H
+    H = np.log2(perplexity)
+
+    return D, P, betas, H
