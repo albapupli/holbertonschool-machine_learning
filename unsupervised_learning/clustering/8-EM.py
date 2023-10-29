3b57571670608f34fb60609ab4f188155e392a25
albapupli <apupli19@epoka.edu.al>
2023-10-29 17:17:17 +0100

    ex8

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -7,6 +7,8 @@ import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
+
+
 def expectation_maximization(X,
                              k,
                              iterations=1000,
