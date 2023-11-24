22d3ac14e49ac61dd8507237c4b21c2a415e3265
albapupli <apupli19@epoka.edu.al>
2023-11-24 22:35:31 +0100

    ex0

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -8,9 +8,6 @@ import tensorflow as tf
 def create_placeholders(nx, classes):
     """
     a function that create placeholders
-    :param nx: the number of feature columns in our data
-    :param classes: the number of classes in our classifier
-    :return: placeholders named x and y, respectively
     """
     return tf.placeholder(float, shape=[None, nx], name='x'), tf.placeholder(
         float, shape=[None, classes], name='y')
