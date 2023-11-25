bed7c2b2d96f725f894de27f15b9fa1712d18f23
albapupli <apupli19@epoka.edu.al>
2023-11-26 00:28:48 +0100

    ex

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python3
 """
-a function  that performs PCA on a dataset
+Performs PCA on a dataset
 """
-
-
 import numpy as np
 
 
 def pca(X, ndim):
     """
-    Calculate the covariance matrix of X
+    a function that performs PCA on a dataset
+    :param X: numpy.ndarray of shape (n, d) where:
+        n is the number of data points
+        d is the number of dimensions in each point
+    :param ndim: the new dimensionality of the transformed X
+    :return: T, a numpy.ndarray of shape (n, ndim) containing the transformed
+    version of X
     """
-    cov_matrix = np.cov(X, rowvar=False)
+    X_mean = X - X.mean(axis=0)
+    u, s, vh = np.linalg.svd(X_mean)
 
-    # Calculate eigenvalues and eigenvectors
-    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-
-    # Sort eigenvalues and eigenvectors in descending order
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[sorted_indices]
-    eigenvectors = eigenvectors[:, sorted_indices]
-
-    # Select the top ndim eigenvectors
-    top_eigenvectors = eigenvectors[:, :ndim]
-
-    # Compute the transformed data
-    T = np.dot(X, top_eigenvectors)
-
-    # Round the values to a reasonable precision
-    T = np.round(T, decimals=4)
+    W = vh.T
+    Wr = W[:, 0:ndim]
+    T = X_mean @ Wr
 
     return T
