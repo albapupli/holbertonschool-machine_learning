6c9968c3379dadd1a8cb03ba1ecfa3fc38bee9a0
albapupli <apupli19@epoka.edu.al>
2023-11-26 00:43:09 +0100

    ex

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,48 +1,26 @@
 #!/usr/bin/env python3
-"""
-Calculates the maximization step in the EM algorithm for a GMM
-"""
+"""Clusterin tasks"""
 import numpy as np
 
 
 def maximization(X, g):
-    """
-    Calculates the maximization step in the EM algorithm for a GMM
-    :param X: numpy.ndarray of shape (n, d) containing the data set
-    :param g: numpy.ndarray of shape (k, n) containing the posterior
-    probabilities for each data point in each cluster
-    :return: pi, m, S, or None, None, None on failure
-        pi is a numpy.ndarray of shape (k,) containing the updated priors for
-        each cluster
-        m is a numpy.ndarray of shape (k, d) containing the updated centroid
-        means for each cluster
-        S is a numpy.ndarray of shape (k, d, d) containing the updated
-        covariance matrices for each cluster
-    """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None, None, None
-    if type(g) is not np.ndarray or len(g.shape) != 2:
-        return None, None, None
+    """Maximization function"""
+    if type(X) is not np.ndarray or type(g) is not np.ndarray:
+        return (None, None, None)
+    if len(X.shape) != 2 or len(g.shape) != 2:
+        return (None, None, None)
     if X.shape[0] != g.shape[1]:
-        return None, None, None
-    cluster = np.sum(g, axis=0)
-    cluster = np.sum(cluster)
-    if int(cluster) != X.shape[0]:
-        return None, None, None
-
+        return (None, None, None)
+    if not np.isclose(np.sum(g, axis=0), 1).all():
+        return (None, None, None)
     n, d = X.shape
-    k, n = g.shape
-
-    # nk is the sum of posterior probabilities
-    nk = np.sum(g, axis=1)
-    # The task here is update priors(pi), mean and covariance(cov)
-    # pi (also call weights) is nk / the total number of points
-    pi = nk / n
-    mean = np.zeros((k, d))
-    cov = np.zeros((k, d, d))
+    k, _ = g.shape
+    pi = g.sum(axis=1) / n
+    m = np.dot(g, X) / g.sum(1)[:, None]
+    S = np.zeros([k, d, d])
     for i in range(k):
-        mean[i] = np.matmul(g[i], X) / nk[i]
-        norm = X - mean[i]
-        cov[i] = np.matmul(g[i] * norm.T, norm) / nk[i]
-
-    return pi, mean, cov
+        ys = X - m[i, :]
+        S[i] = (g[i, :, None, None] *
+                np.matmul(ys[:, :, None], ys[:, None, :])).sum(axis=0)
+    S = S / g.sum(axis=1)[:, None, None]
+    return (pi, m, S)
