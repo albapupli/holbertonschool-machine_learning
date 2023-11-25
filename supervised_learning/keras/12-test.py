4755ba3ef540c6b7d489a91d8565f1cf997dd9a7
albapupli <apupli19@epoka.edu.al>
2023-11-26 00:00:45 +0100

    xe

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 """
-this function tests a neural network
+Tests a Neural Network
 """
-
-
 import tensorflow.keras as K
 
 
 def test_model(network, data, labels, verbose=True):
     """
-    testing a neural network
+    a function that tests a NN
+    :param network: the network model to test
+    :param data: the input data to test the model with
+    :param labels: the correct one-hot labels of data
+    :param verbose: a boolean that determines if output should be printed
+    during the testing process
+    :return: the loss and accuracy of the model with the testing data,
+    respectively
     """
-    loss, accuracy = network.evaluate(x=data,
-                                      y=labels,
-                                      verbose=verbose)
-    return loss, accuracy
+    return network.evaluate(data, labels, verbose=verbose)
