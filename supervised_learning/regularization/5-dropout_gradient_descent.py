67b5c155e5f7b467071c74f5741632e025aac1b5
albapupli <apupli19@epoka.edu.al>
2023-11-25 23:43:00 +0100

    ex

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,51 +1,45 @@
 #!/usr/bin/env python3
 """
-this function updates the weights
-of a neural network with Dropout regularization using gradient descent
+Conducts gradient descent using Dropout:
 """
-
-
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    drop out gradient descent function
+    a function that conducts gradient descent using dropout
+    :param Y: one-hot numpy.ndarray of shape (classes, m) that contains the
+    correct labels for the data
+        classes is the number of classes
+        m is the number of data points
+    :param weights: a dictionary of the weights and biases of the neural
+    network
+    :param cache: a dictionary of the outputs and dropout masks of each layer
+    :param alpha: the learning rate
+    :param keep_prob: the probability that a node will be kept
+    :param L: the number of layers in the network
+    :return: no return
     """
     m = Y.shape[1]
-    back = {}
-
-    for index in range(L, 0, -1):
-        A = cache["A{}".format(index - 1)]
-        if index == L:
-            # if last layer, calculate dz value as A - Y
-            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
-            dz = back["dz{}".format(index)]
-
+    for i in reversed(range(L)):
+        # create keys to access weights(W), biases(b) and store in cache
+        key_w = 'W' + str(i + 1)
+        key_b = 'b' + str(i + 1)
+        key_cache = 'A' + str(i + 1)
+        key_cache_dw = 'A' + str(i)
+        # Activation
+        A = cache[key_cache]
+        A_dw = cache[key_cache_dw]
+        if i == L - 1:
+            dz = A - Y
+            W = weights[key_w]
         else:
-            # retreive previous dz and current activation
-            dz_prev = back["dz{}".format(index + 1)]
-            A_current = cache["A{}".format(index)]
-
-            # calculate current dz
-            back["dz{}".format(index)] = (
-                np.matmul(W_prev.transpose(), dz_prev) *
-                (A_current * (1 - A_current)))
-
-            # multiply by dropout mask and divide by keep probability
-            dz = back["dz{}".format(index)]
-            dz *= cache["D{}".format(index)]
-            dz /= keep_prob
-
-        # calculate weights and bias
-        dW = (1 / m) * (np.matmul(dz, A.transpose()))
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-
-        # store for next interation
-        W_prev = weights["W{}".format(index)]
-
-        # modify weights and bias
-        weights["W{}".format(index)] = (
-            weights["W{}".format(index)] - (alpha * dW))
-        weights["b{}".format(index)] = (
-            weights["b{}".format(index)] - (alpha * db))
+            da = 1 - (A * A)
+            dz = np.matmul(W.T, dz)
+            dz = dz * da * cache["D{}".format(i + 1)]
+            dz = dz / keep_prob
+            W = weights[key_w]
+        dw = np.matmul(A_dw, dz.T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        weights[key_w] = weights[key_w] - alpha * dw.T
+        weights[key_b] = weights[key_b] - alpha * db
