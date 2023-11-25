30fc60e1fb393a7c407a797375078e133f2de00e
albapupli <apupli19@epoka.edu.al>
2023-11-25 22:36:18 +0100

    ex2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,39 +1,26 @@
 #!/usr/bin/env python3
-"""
-Prints the location of a specific Github user
-"""
-
-
+""" data collection from APIs """
 import sys
-import requests
-from datetime import datetime, timedelta
+# import requests
+from requests import get
+import time
 
-def get_user_location(api_url):
-    """Function to get the location of userf"""
-    response = requests.get(api_url)
-
-    if response.status_code == 404:
-        print("Not found")
-    elif response.status_code == 403:
-        reset_time = int(response.headers['X-Ratelimit-Reset'])
-        current_time = int(datetime.now().timestamp())
-        wait_time = max(0, reset_time - current_time)
-        wait_minutes = wait_time // 60
-        print(f"Reset in {wait_minutes} min")
-    elif response.status_code == 200:
-        user_data = response.json()
-        location = user_data.get('location')
-        if location:
-            print(location)
-        else:
-            print("Location not available")
-    else:
-        print("Unexpected error")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py <API_URL>")
-        sys.exit(1)
+    """ Script that prints the location of a specific user """
 
-    api_url = sys.argv[1]
-    get_user_location(api_url)
+    # https://api.github.com/users/holbertonschool
+    url = sys.argv[1]
+    # user is passed as first argument of the script with the full API URL
+    request = get(url, params={'Accept': "application/vnd.github.v3+json"})
+
+    if request.status_code == 200:
+        print(request.json()["location"])
+
+    if request.status_code == 404:
+        print("Not found")
+
+    if request.status_code == 403:
+        limit = request.headers["X-Ratelimit-Reset"]
+        x = int((int(limit) - int(time.time())) / 60)
+        print("Reset in " + str(x) + " min")
