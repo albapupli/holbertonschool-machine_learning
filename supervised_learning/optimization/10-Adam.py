66fde0f9aeb694a41fa894b502428fa9d0bd9744
albapupli <apupli19@epoka.edu.al>
2023-11-25 23:22:59 +0100

    ex

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -1,42 +1,23 @@
 #!/usr/bin/env python3
 """
-this function creates a training operation for nn
-using Adam optimization alg
+Updates a variable using the Adam optimization algorithm with tensorflow
 """
-
-
 import tensorflow as tf
-
-
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """
-    training operation using RMSProp opt
-    """
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
-                                          decay=beta2, epsilon=epsilon)
-    train_op = optimizer.minimize(loss)
-    return train_op
-
-
-def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
-    """
-    update a var in place using Adam opt
-    """
-    v_dW = (beta1 * v) + ((1 - beta1) * grad)
-    s_dW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
-    v_dW_c = v_dW / (1 - (beta1 ** t))
-    s_dW_c = s_dW / (1 - (beta2 ** t))
-    var -= alpha * (v_dW_c / (epsilon + (s_dW_c ** (1 / 2))))
-    return var, v_dW, s_dW
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """
-    training operation of nn using Adam opt
+    a function that optimizes using Adam with tensorflow
+    :param alpha: the learning rate
+    :param beta1: the weight used for the first moment
+    :param beta2: the weight used for the second moment
+    :param epsilon: small number to avoid division by zero
+    :param var: numpy.ndarray containing the variable to be updated
+    :param grad: numpy.ndarray containing the gradient of var
+    :param v: the previous first moment of var
+    :param s: the previous second moment of var
+    :param t: the time step used for bias correction
+    :return: the updated variable, the new first moment, and the new second
+    moment, respectively
     """
-    optimizer = tf.keras.optimizers.RMSprop(
-        learning_rate=alpha,
-        beta_1=beta1,
-        beta_2=beta2,
-        epsilon=epsilon)
-    return optimizer.minimize(loss)
+    return tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon).minimize(loss)
