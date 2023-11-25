9951cfc41ae0fc8a27aa39913926e94cc2ea9c1e
albapupli <apupli19@epoka.edu.al>
2023-11-25 22:15:26 +0100

    ex102

--- pipeline/databases/102-need_meeting.sql
+++ pipeline/databases/102-need_meeting.sql
@@ -1,1 +1,6 @@
--- No table for a meeting
+-- view
+CREATE OR REPLACE VIEW need_meeting AS
+SELECT name FROM students
+WHERE score < 80
+AND last_meeting IS NULL OR 
+	(MONTH(CURDATE()) - MONTH(last_meeting)) > 1;
