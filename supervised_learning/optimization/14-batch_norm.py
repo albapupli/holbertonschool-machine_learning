26c86a3942194d2aebe195dd82e1275fb5ce0d18
albapupli <apupli19@epoka.edu.al>
2023-11-25 23:32:23 +0100

    ex

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -9,13 +9,30 @@ import tensorflow.compat.v1 as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
-    """Batch normalization upgraded"""
-    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    model = tf.layers.Dense(units=n, name="layer", kernel_initializer=W)
-    X = model(prev)
-    beta = tf.Variable(tf.constant(0.0, shape=[n]), trainable=True)
-    gamma = tf.Variable(tf.constant(1.0, shape=[n]), trainable=True)
-    mean, variance = tf.nn.moments(X, [0])
-    norm = tf.nn.batch_normalization(X, mean, variance, offset=beta,
-                                     scale=gamma, variance_epsilon=1e-8)
-    return activation(norm)
+    """
+    a function that uses batch normalization with tensorflow
+    :param prev: the activated output of the previous layer
+    :param n: the number of nodes in the layer to be created
+    :param activation: the activation function that should be used on the
+    output of the layer
+    :return: a tensor of the activated output for the layer
+    """
+    # Layers
+    k_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    output = tf.layers.Dense(units=n, kernel_initializer=k_init)
+    Z = output(prev)
+
+    # Gamma and Beta initialization
+    gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]),
+                        name="gamma")
+    beta = tf.Variable(initial_value=tf.constant(0.0, shape=[n]), name="beta")
+
+    # Batch normalization
+    mean, var = tf.nn.moments(Z, axes=0)
+    b_norm = tf.nn.batch_normalization(Z, mean, var, offset=beta,
+                                       scale=gamma,
+                                       variance_epsilon=1e-8)
+    if activation is None:
+        return b_norm
+    else:
+        return activation(b_norm)
