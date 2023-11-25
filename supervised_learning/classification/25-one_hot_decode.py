ae5b1cbdc9c6d00aa91c00ea5e74b5a2b8ead8b5
albapupli <apupli19@epoka.edu.al>
2023-11-25 23:12:07 +0100

    ex

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Deep neural network"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """Function that converts a one-hot matrix into a vector of labels"""
+    if len(one_hot) == 0 or type(one_hot) is not np.ndarray:
+        return None
+    if len(one_hot.shape) != 2:
+        return None
+    return np.argmax(one_hot, axis=0)
