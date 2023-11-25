487a37b3dd232ba436dc8e119bf5ca66494f6167
albapupli <apupli19@epoka.edu.al>
2023-11-26 00:17:14 +0100

    ex

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-"""Bag of words embedding matrix"""
+""" Bag Of Words """
+
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
-    """Bag of words"""
+    """ creates a bag of words embedding matrix """
     vectorizer = CountVectorizer(vocabulary=vocab)
     X = vectorizer.fit_transform(sentences)
-    return (X.toarray(), vectorizer.get_feature_names())
+
+    return X.toarray(), vectorizer.get_feature_names()
