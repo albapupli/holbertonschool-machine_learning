a8f48c6dd0e0359080e38cb8f47ed9cf3968357d
albapupli <apupli19@epoka.edu.al>
2023-11-26 02:31:37 +0100

    ex

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
 """
-Prints the location of a specific Github user
+Rate me is you can!
 """
-import requests
-import sys
-import time
 
+import time
+import sys
+import requests
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         exit()
     url = sys.argv[1]
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    r = requests.get(url, headers=headers)
-
-    if r.status_code == 200:
-        print(r.json()['location'])
-
-    if r.status_code == 404:
-        print("Not found")
-
-    if r.status_code == 403:
-        rate_limit = int(r.headers['X-Ratelimit-Reset'])
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        print(res.json()['location'])
+    elif res.status_code == 403:
+        rate_limit = int(res.headers['X-Ratelimit-Reset'])
         now = int(time.time())
         minutes = int((rate_limit - now) / 60)
         print("Reset in {} min".format(minutes))
+    elif res.status_code == 404:
+        print('Not found')
