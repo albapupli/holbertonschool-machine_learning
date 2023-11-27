933ead1164089d5fd35bc6914613a115f578a2a1
albapupli <57274211+albapupli@users.noreply.github.com>
2023-11-27 18:39:11 +0100

    ex

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -5,6 +5,7 @@ Defines a function that creates the forward propagation graph for the neural net
 
 
 import tensorflow.compat.v1 as tf
+create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
