05dd85c89baca45c74824c4bc675f94652646904
albapupli <57274211+albapupli@users.noreply.github.com>
2023-11-27 18:36:51 +0100

    ex

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -9,13 +9,13 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """
-    Creates a layer for neural network
+    a function that create layers
+    :param prev: the tensor output of the previous layer
+    :param n: the number of nodes in the layer to create
+    :param activation: is the activation function that the layer should use
+    :return: the tensor output of the layer
     """
-    weights_initializer = tf.contrib.layers.variance_scaling_initializer(
-                          mode="FAN_AVG")
-    layer = tf.layers.Dense(
-        n,
-        activation=activation,
-        name="layer",
-        kernel_initializer=weights_initializer)
-    return (layer(prev))
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(n, activation=activation, kernel_initializer=init,
+                            name="layer")
+    return layer(prev)
